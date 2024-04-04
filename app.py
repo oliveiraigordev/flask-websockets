31a9e5b5ad7398658c5ac069084a4ccd03258dc9
Igor Oliveira <44407469+oliveiraigordev@users.noreply.github.com>
2024-04-04 17:44:25 +0000

    criacao de banco de dados e tabelas

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, jsonify
+from repository.database import db
+from routes.payments import payment_bp
+from models.payment import Payment
 
 
 app = Flask(__name__)
-
+app.config.from_pyfile('settings.py')
 app.register_blueprint(payment_bp)
 
+db.init_app(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
