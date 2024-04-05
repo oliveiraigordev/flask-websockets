83eae40cb55343195792954d8f545e75d5483c44
Igor Oliveira <44407469+oliveiraigordev@users.noreply.github.com>
2024-04-04 21:13:04 -0300

    inclusao de teste unitario

---
+++ tests/test_pix.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append('../')
+
+import pytest
+import os
+from payments.pix import Pix
+
+
+def test_pix_create_payment():
+    pix_instance = Pix()
+    payment_info = pix_instance.create_payment(base_dir='../')
+    assert 'bank_payment_id' in payment_info
+    assert 'qr_code_path' in payment_info
+    qr_code_path = payment_info['qr_code_path']
+    assert os.path.isfile(f'../static/qrcode_img/{qr_code_path}.png')
