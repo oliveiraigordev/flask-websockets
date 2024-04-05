c3033d083f04354152374954597bbcd5f596a5d1
Igor Oliveira <44407469+oliveiraigordev@users.noreply.github.com>
2024-04-04 21:26:34 -0300

    :bug: corrigido bug de criacao de pasta para salvar qrcode

--- payments/pix.py
+++ payments/pix.py
@@ -1,16 +1,20 @@
 import uuid
 import qrcode
+import os
 
 
 class Pix:
     def __init__(self):
         pass
 
-    def create_payment(self):
+    def create_payment(self, base_dir=''):
         bank_payment_id = str(uuid.uuid4())
         hash_payment = f'hash_payment_{bank_payment_id}'
         img = qrcode.make(hash_payment)
-        img.save(f'static/qrcode_img/qr_code_payment_{bank_payment_id}.png')
+        path_to_save = f'{base_dir}static/qrcode_img'
+        if not os.path.exists(path_to_save):
+            os.makedirs(path_to_save)
+        img.save(f'{path_to_save}/qr_code_payment_{bank_payment_id}.png')
 
         return {
             "bank_payment_id": bank_payment_id,
